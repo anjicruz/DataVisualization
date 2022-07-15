c107dd06e2e0174ebaed99c350fd3a88fb128702
anjicruz <anji_cruz@msn.com>
2022-07-15 22:59:39 +0800

    Defining Custom Colors 7/15/2022 1059pm

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,9 +1,12 @@
 from shutil import which
 import matplotlib.pyplot as plt
 
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=200)
+ax.scatter(x_values, y_values, c=(0,0.8,0), s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -11,6 +14,5 @@ ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
-ax.tick_params(axis='both', which='major', labelsize=14)
-
+ax.axis([0,1100,0,1100000])
 plt.show()
